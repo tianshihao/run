0b1d6da302de4e006d087d0739800abce24afba4
Shihao Tian <shihao.tian@outlook.com>
2022-09-10 01:01:34 +0800

    feat(replica): add cpp-primer

---
+++ replica/cpp-primer/chapter15/query/src/and_query.cpp
@@ -0,0 +1,14 @@
+#include "../include/and_query.h"
+
+#include <algorithm>
+
+QueryResult AndQuery::Eval(TextQuery const& text) const {
+  auto left{lhs.Eval(text)}, right{rhs.Eval(text)};
+
+  auto ret_lines{std::make_shared<std::set<line_no>>()};
+
+  std::set_intersection(left.Begin(), left.End(), right.Begin(), right.End(),
+                        std::inserter(*ret_lines, ret_lines->begin()));
+
+  return QueryResult(Rep(), ret_lines, left.GetFile());
+}
