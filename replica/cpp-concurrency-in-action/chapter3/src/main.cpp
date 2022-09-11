798e3cdd4646ba649a4b4de77f701f3f0c4d7c3a
Tian Shihao <shihao.tian@outlook.com>
2022-09-11 23:32:31 +0800

    feat(replica): implementation two ways of avoids deadlock

--- replica/cpp-concurrency-in-action/chapter3/src/main.cpp
+++ replica/cpp-concurrency-in-action/chapter3/src/main.cpp
@@ -2,6 +2,8 @@
 #include <list>
 #include <mutex>
 
+/**************** Listing 3.1 Protecting a list with a mutex. ****************/
+
 std::list<int> some_list;
 std::mutex some_mutex;
 
@@ -24,12 +26,14 @@ bool ListContains(int value_to_find) {
          some_list.end();
 }
 
+/***** Listing 3.2 Accidentally passing out a reference to protected data *****/
+
 class SomeData {
   int a;
   std::string b;
 
  public:
-  void DoSometing();
+  void DoSomething() {}
 };
 
 class DataWrapper {
@@ -64,3 +68,74 @@ void Foo() {
   // Unprotected access to protected data.
   unprotected->DoSomething();
 }
+
+/*** Listing 3.6 Using std::lock() and std::lock_guard in a swap operation ***/
+
+class SomeBigObject;
+
+class SomeBigObject {
+ private:
+  std::string a_big_chunk[1000];
+};
+
+void Swap(SomeBigObject& lhs, SomeBigObject& rhs);
+
+class X {
+ private:
+  SomeBigObject some_detail;
+  std::mutex m;
+
+ public:
+  X(SomeBigObject const& sd) : some_detail{sd} {}
+
+  friend void Swap(X& lhs, X& rhs) {
+    // Ensure lhs and rhs are different instances.  Because attemping to acquire
+    // a lock on st::mutex when you already hold it is undefined behavior.
+    if (&lhs == &rhs) {
+      return;
+    }
+    // The call to std::lock() locks the two mutexes.  Don't worry, If std::lock
+    // has successfully acquired a lock on a mutex and an exception thrown when
+    // it tries to acquire a lock on the other mutex, this first lock is
+    // released automatically.  Yeah, all-or-nothing.
+    std::lock(lhs.m, rhs.m);
+    // ? Why still needs the lock_guard? Isn't std::lock() has already locked
+    // ? the two mutexes?
+
+    // Then the two std::lock_guard instances are constructed, one
+    // for each mutex.  The std::adopt_lock parameter is supplied in addition to
+    // the mutext to indecate to the std::lock_guard objects that the mutexes
+    // are already locked, and they should adopt the ownership of existing lock
+    // on the mutex rather than attemp to lock the mutex in the constructor.
+    std::lock_guard<std::mutex> lock_a{lhs.m, std::adopt_lock};
+    std::lock_guard<std::mutex> lock_b{rhs.m, std::adopt_lock};
+    Swap(lhs.some_detail, rhs.some_detail);
+  }
+};
+
+/***** Listing 3.9 Using std::lock() std::unique_lock in a swap operation *****/
+
+class Y {
+ private:
+  SomeBigObject some_detail;
+  std::mutex m;
+
+ public:
+  Y(SomeBigObject const& sd) : some_detail{sd} {}
+  friend void Swap(Y& lhs, Y& rhs) {
+    if (&lhs == &rhs) {
+      return;
+    }
+    // std::defer_lock leaves mutexes unlocked.
+    std::unique_lock<std::mutex> lock_a{lhs.m, std::defer_lock};
+    std::unique_lock<std::mutex> lock_b{rhs.m, std::defer_lock};
+    // Mutexes are locked here.
+    std::lock(lock_a, lock_b);
+
+    Swap(lhs.some_detail, rhs.some_detail);
+
+    return;
+  }
+};
+
+int main(int argc, char** argv) { return 0; }
