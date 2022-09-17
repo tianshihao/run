6f3ff0c46eb6b24d0cfa8af17d302823038774b0
Tian Shihao <shihao.tian@outlook.com>
2022-09-17 18:08:16 +0800

    feat(replica): using call_once to solve the data initialization

--- replica/cpp-concurrency-in-action/chapter3/src/main.cpp
+++ replica/cpp-concurrency-in-action/chapter3/src/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <list>
+#include <memory>
 #include <mutex>
 
 /**************** Listing 3.1 Protecting a list with a mutex. ****************/
@@ -52,9 +53,9 @@ class DataWrapper {
   }
 };
 
-SomeData* unprotected;
+SomeData *unprotected;
 
-void MaliciousFunction(SomeData& protected_data) {
+void MaliciousFunction(SomeData &protected_data) {
   unprotected = &protected_data;
 
   return;
@@ -78,7 +79,7 @@ class SomeBigObject {
   std::string a_big_chunk[1000];
 };
 
-void Swap(SomeBigObject& lhs, SomeBigObject& rhs);
+void Swap(SomeBigObject &lhs, SomeBigObject &rhs);
 
 class X {
  private:
@@ -86,9 +87,9 @@ class X {
   std::mutex m;
 
  public:
-  X(SomeBigObject const& sd) : some_detail{sd} {}
+  X(SomeBigObject const &sd) : some_detail{sd} {}
 
-  friend void Swap(X& lhs, X& rhs) {
+  friend void Swap(X &lhs, X &rhs) {
     // Ensure lhs and rhs are different instances.  Because attemping to acquire
     // a lock on st::mutex when you already hold it is undefined behavior.
     if (&lhs == &rhs) {
@@ -115,14 +116,14 @@ class X {
 
 /***** Listing 3.9 Using std::lock() std::unique_lock in a swap operation *****/
 
-class Y {
+class Z {
  private:
   SomeBigObject some_detail;
   std::mutex m;
 
  public:
-  Y(SomeBigObject const& sd) : some_detail{sd} {}
-  friend void Swap(Y& lhs, Y& rhs) {
+  Z(SomeBigObject const &sd) : some_detail{sd} {}
+  friend void Swap(Z &lhs, Z &rhs) {
     if (&lhs == &rhs) {
       return;
     }
@@ -138,4 +139,93 @@ class Y {
   }
 };
 
-int main(int argc, char** argv) { return 0; }
+/***** Listing 3.10 Locking one mutex at a time in a comparsion operator *****/
+
+class Y {
+ private:
+  int some_detail;
+  mutable std::mutex m;
+  int GetDetail() const {
+    std::lock_guard<std::mutex> lock_a{m};
+    return some_detail;
+  }
+
+ public:
+  Y(int sd) : some_detail{sd} {}
+  // This is a bad case.  Note, however, that as well as reducing the locking
+  // periods so that only one lock is held at a time, this has subtly changed
+  // the semantics of the operation compared to holding both locks together.
+  // The two values could have been changed in any way in between the two reads.
+  // ! The values could have been swapped in between #2 and #3, and the
+  // comparsion ! might returns true. ! If you don't hold the required locks for
+  // the entire duration of an ! operation, you're  exposing yourself to race
+  // comditions.
+  friend bool operator==(Y const &lhs, Y const &rhs) {
+    if (&lhs == &rhs) {
+      return true;
+    }
+
+    // GetDetail() retrieves the value while protecting it with a lock.
+    // ! There is only a lock at a time, Not the entire duration.
+    // #2
+    int const lhs_value{lhs.GetDetail()};
+    // #3
+    int const rhs_value{rhs.GetDetail()};
+
+    return lhs_value == rhs_value;
+  }
+};
+
+/*** Rewritting Listing 3.11 Thread-safe lazy initialization using a mutext ***/
+
+std::shared_ptr<SomeData> resource_ptr;
+std::once_flag resource_flag;
+void InitResource() {
+  resource_ptr.reset(new SomeData);
+  return;
+}
+
+void Foo() {
+  // Initialization is called exactly once.
+  std::call_once(resource_flag, InitResource);
+  resource_ptr->DoSomething();
+}
+
+/**
+ * Listing 3.12 Thread-safe lazy initailization of a class member using
+ * std::call_once
+ */
+
+class ConnectionHandle;
+class ConnectionInfo;
+class ConnectionManager;
+class DataPacket;
+
+class XRewritten {
+ private:
+  ConnectionInfo connection_details;
+  ConnectionHandle connection;
+  std::once_flag connection_init_flag;
+  void OpenConnection() {
+    connection = ConnectionManager.Open(connection_details);
+    return;
+  }
+
+ public:
+  XRewritten(ConnectionInfo const &connection_details_)
+      : connection_details{connection_details_} {}
+
+  void SendData(DataPacket const &data) {
+    std::call_once(connection_init_flag, &XRewritten::OpenConnection, this);
+    connection.SendData(data);
+
+    return;
+  }
+
+  DataPacket ReceiveData() {
+    std::call_once(connection_init_flag, &XRewritten::OpenConnection, this);
+    return connection.ReceiveData();
+  }
+};
+
+int main(int argc, char **argv) { return 0; }
