86793dd14c621742196c449a82694f55ba8acdef
Shihao Tian <shihao.tian@outlook.com>
2022-10-08 18:09:35 +0800

    refactor(replica): move code into separator header

--- replica/cpp-primer/chapter16/src/main.cpp
+++ replica/cpp-primer/chapter16/src/main.cpp
@@ -7,18 +7,10 @@
 
 #include "blob.h"
 #include "blob_ptr.h"
+#include "find.h"
 #include "screen.h"
 
-/********************************** Ex 16.4 **********************************/
-template <typename Itertor, typename Value>
-auto Find(Itertor first, Itertor last, Value const& val) {
-  for (; first != last && *first != val; ++first) {
-  }
-
-  return first;
-}
-
-void TestEx_16_4() {
+void TestFind() {
   std::vector<int> vct{1, 2, 3, 4, 5, 6, 7, 8, 9};
   auto is_in_vct{vct.cend() != Find(vct.cbegin(), vct.cend(), 5)};
   std::cout << (is_in_vct ? "found\n" : "not found\n");
@@ -62,7 +54,7 @@ void TestScreen() {
   return;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   // TestBlob();
   // TestBlobPtr();
   TestScreen();
