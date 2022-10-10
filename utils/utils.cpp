fc0818896513a8aed4c7b8c387ef8dadb5f53490
Shihao Tian <shihao.tian@outlook.com>
2022-10-10 15:39:27 +0800

    feat(utils): add new module utils

---
+++ utils/utils.cpp
@@ -0,0 +1,28 @@
+#include "utils.h"
+
+namespace utils {
+int PrintUsage() {
+  std::cout << "Usage: example [command] [subcommand]\n"
+               "\n"
+               "Run specified example.\n"
+               "\n"
+               "Command:\n"
+               "    calculate [add|average|complex(default)]\n"
+               "    config\n"
+               "    loop [loop1|loop2(default)]\n"
+               "    performance\n"
+               "    reschedule\n"
+               "    json_config\n"
+               "    flusher\n"
+               "    group\n"
+               "    help \n"
+               "\n"
+               "Extra:\n"
+               " \n"
+               "You can use tools such as valgrind to check memory leaks\n"
+               "valgrind --leak-check=full"
+               " --show-leak-kinds=all example [command] [subcommand]  "
+            << std::endl;
+  return 0;
+}
+}  // namespace utils
---
+++ utils/utils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+
+namespace utils {
+int PrintUsage();
+}  // namespace utils
