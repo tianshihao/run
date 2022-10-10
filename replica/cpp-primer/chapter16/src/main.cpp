6472041d49d752f9efcc618628424dd73eecef41
Shihao Tian <shihao.tian@outlook.com>
2022-10-10 17:34:34 +0800

    fix(replica): make compatible with the input field of launch.json

--- replica/cpp-primer/chapter16/src/main.cpp
+++ replica/cpp-primer/chapter16/src/main.cpp
@@ -10,7 +10,7 @@ static std::map<std::string, fn> examples{
     {"blob", TestBlob}, {"blobptr", TestBlobPtr}, {"screen", TestScreen}};
 
 int main(int argc, char **argv) {
-  if (argc < 2) {
+  if (2 > argc || 0 == *argv[1]) {
     utils::PrintUsage();
   } else {
     examples[argv[1]](argc - 1, argv + 1);
