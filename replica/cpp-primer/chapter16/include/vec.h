2c15d3a38705085f2fd3b4ab3501d4ff59fa9870
Shihao Tian <shihao.tian@outlook.com>
2022-10-29 14:07:47 +0800

    style(replica): typo of template Vec

--- replica/cpp-primer/chapter16/include/vec.h
+++ replica/cpp-primer/chapter16/include/vec.h
@@ -16,9 +16,11 @@
 #include <initializer_list>
 #include <memory>
 
+// Forward declaration.
 template <typename>
 class Vec;
 
+// Forward feclaration for friends.
 template <typename T>
 bool operator==(const Vec<T>&, const Vec<T>&);
 template <typename T>
@@ -34,6 +36,35 @@ bool operator>=(const Vec<T>&, const Vec<T>&);
 
 template <typename T>
 class Vec {
+ public:
+  Vec() noexcept;
+  Vec(std::initializer_list<T>) noexcept;
+  Vec(Vec<T> const&) noexcept;
+  Vec& operator=(Vec<T> const&) noexcept;
+  Vec(Vec<T>&&) noexcept;
+  Vec& operator=(Vec<T>&&) noexcept;
+  ~Vec() noexcept;
+
+  void PushBack(T const&);
+  // TODO, emplace back operation if needed here.
+
+  std::size_t Size() const { return rear - head; }
+  std::size_t Capacity() const { return boundary - head; }
+
+  T* Begin() const { return head; }
+  T* End() const { return rear; }
+
+  T& At(std::size_t pos) { return *(head + pos); }
+  T const& At(std::size_t pos) const { return *(head + pos); }
+
+  T& operator[](std::size_t pos) { return head[pos]; }
+  T const& operator[](std::size_t pos) const { return head[pos]; }
+
+  void Reserve(std::size_t);
+
+  void Resize(std::size_t);
+  void Resize(std::size_t, T const&);
+
   friend bool operator==<T>(const Vec<T>&, const Vec<T>&);
   friend bool operator!=<T>(const Vec<T>&, const Vec<T>&);
   // clang-format off
@@ -43,47 +74,23 @@ class Vec {
   friend bool operator<=<T>(const Vec<T>&, const Vec<T>&);
   friend bool operator>=<T>(const Vec<T>&, const Vec<T>&);
 
- public:
-  Vec() noexcept;
-  Vec(std::initializer_list<T>) noexcept;
-  Vec(Vec<T> const&) noexcept;
-  Vec& operator=(Vec<T> const&) noexcept;
-  Vec(Vec<T>&&) noexcept;
-  Vec& operator=(Vec<T>&&) noexcept;
-  ~Vec();
-
-  void PushBack(T const&);
-  size_t Size() const { return first_free - elements; }
-  size_t Capacity() const { return cap - elements; }
-  T* Begin() const { return elements; }
-  T* End() const { return first_free; }
-
-  T& At(size_t pos) { return *(elements + pos); }
-  T const& At(size_t pos) const { return *(elements + pos); }
-  T& operator[](size_t pos) { return elements[pos]; }
-  T const& operator[](size_t pos) const { return elements[pos]; }
-
-  void Reserve(size_t);
-  void Resize(size_t);
-  void Resize(size_t, T const&);
-
  private:
-  std::pair<T*, T*> AllocNCopy(T const*, T const*);
-  void Free();
   void CheckNAlloc();
-  void Reallocate();
-  void AllocNMove(size_t);
   void RangeInitialize(T const*, T const*);
+  std::pair<T*, T*> AllocNCopy(T const*, T const*);
+  void AllocNMove(std::size_t);
+  void Reallocate();
+  void Free() noexcept;
 
  private:
-  T* elements;
-  T* first_free;
-  T* cap;
+  T* head;
+  T* rear;
+  T* boundary;
   std::allocator<T> alloc;
 };
 
 template <typename T>
-Vec<T>::Vec() noexcept : elements(nullptr), first_free(nullptr), cap(nullptr) {}
+Vec<T>::Vec() noexcept : head(nullptr), rear(nullptr), boundary(nullptr) {}
 
 template <typename T>
 Vec<T>::Vec(std::initializer_list<T> il) noexcept {
@@ -97,45 +104,54 @@ Vec<T>::Vec(Vec<T> const& rhs) noexcept {
 
 template <typename T>
 Vec<T>& Vec<T>::operator=(Vec<T> const& rhs) noexcept {
-  auto data{AllocNCopy(rhs.Begin(), rhs.End())};
+  auto new_head{AllocNCopy(rhs.Begin(), rhs.End())};
+
+  // Different from copy constuctor, the copy assignment operator must free its
+  // resource first, then take the new data.
   Free();
-  elements = data.first;
-  first_free = data.second;
+  head = new_head.first;
+  rear = new_head.second;
+
   return *this;
 }
 
 template <typename T>
 Vec<T>::Vec(Vec<T>&& rhs) noexcept
-    : elements(rhs.elements), first_free(rhs.first_free), cap(rhs.cap) {
-  rhs.elements = rhs.first_free = rhs.cap = nullptr;
+    : head(rhs.head), rear(rhs.rear), boundary(rhs.boundary) {
+  rhs.head = rhs.rear = rhs.boundary = nullptr;
 }
 
 template <typename T>
 Vec<T>& Vec<T>::operator=(Vec<T>&& rhs) noexcept {
   if (this != &rhs) {
+    // Same with copy assignment operator.
     Free();
-    elements = rhs.elements;
-    first_free = rhs.first_free;
-    cap = rhs.cap;
-    rhs.elements = rhs.first_free = rhs.cap = nullptr;
+
+    // Steal.
+    head = rhs.head;
+    rear = rhs.rear;
+    boundary = rhs.boundary;
+
+    rhs.head = rhs.rear = rhs.boundary = nullptr;
   }
+
   return *this;
 }
 
 template <typename T>
-Vec<T>::~Vec() {
+Vec<T>::~Vec() noexcept {
   Free();
 }
 
 template <typename T>
 void Vec<T>::PushBack(const T& from) {
   CheckNAlloc();
-  alloc.construct(first_free++, from);
+  alloc.construct(rear++, from);
   return;
 }
 
 template <typename T>
-void Vec<T>::Reserve(size_t new_cap) {
+void Vec<T>::Reserve(std::size_t new_cap) {
   if (new_cap <= Capacity()) {
     return;
   }
@@ -144,23 +160,29 @@ void Vec<T>::Reserve(size_t new_cap) {
 }
 
 template <typename T>
-void Vec<T>::Resize(size_t cnt) {
-  Resize(cnt, T());
+void Vec<T>::Resize(std::size_t sz) {
+  Resize(sz, T());
   return;
 }
 
 template <typename T>
-void Vec<T>::Resize(size_t cnt, T const& s) {
-  if (cnt > Size()) {
-    if (cnt > Capacity()) {
-      Reserve(cnt * 2);
-      for (size_t i{Size()}; i != cnt; ++i) {
-        alloc.construct(first_free++, s);
+void Vec<T>::Resize(std::size_t sz, T const& s) {
+  if (sz > Size()) {
+    // Need to reserve.
+    if (sz > Capacity()) {
+      Reserve(sz * 2);
+      for (std::size_t i{Size()}; i != sz; ++i) {
+        // Initial the new memory.
+        alloc.construct(rear++, s);
       }
     }
-  } else if (cnt < Size()) {
-    while (first_free != elements + cnt) {
-      alloc.destory(--first_free);
+    // Nothing to do.
+    else {
+    }
+  } else if (sz < Size()) {
+    // Destory the rear data but do not free memory.
+    while (rear != head + sz) {
+      alloc.destory(--rear);
     }
   } else {
   }
@@ -170,16 +192,15 @@ void Vec<T>::Resize(size_t cnt, T const& s) {
 
 template <typename T>
 std::pair<T*, T*> Vec<T>::AllocNCopy(T const* b, T const* e) {
-  auto data{alloc.allocate(e - b)};
-  return {data, std::uninitialized_copy(b, e, data)};
+  auto result{alloc.allocate(e - b)};
+  return {result, std::uninitialized_copy(b, e, result)};
 }
 
 template <typename T>
-void Vec<T>::Free() {
-  if (elements) {
-    std::for_each(elements, first_free,
-                  [this](T& rhs) { alloc.destory(&rhs); });
-    alloc.deallocate(elements, cap - elements);
+void Vec<T>::Free() noexcept {
+  if (head) {
+    std::for_each(head, rear, [this](T& rhs) { alloc.destory(&rhs); });
+    alloc.deallocate(head, boundary - head);
   }
 
   return;
@@ -202,26 +223,32 @@ void Vec<T>::Reallocate() {
 }
 
 template <typename T>
-void Vec<T>::AllocNMove(size_t new_cap) {
-  auto new_data{alloc.allocate(new_cap)};
-  auto dest{new_data};
-  auto elem{elements};
-  for (size_t i{0}; i != Size(); ++i) {
-    alloc.construct(dest++, std::move(*elem++));
+void Vec<T>::AllocNMove(std::size_t new_cap) {
+  auto new_head{alloc.allocate(new_cap)};
+
+  auto dest2constuct{new_head};
+  auto elem2move{head};
+  for (std::size_t i{0}; i != Size(); ++i) {
+    // Move.
+    alloc.construct(dest2constuct++, std::move(*elem2move++));
   }
+
+  // Free the origin data.
+  // TODO, I moved the data above, why do I need free origin data here?
   Free();
-  elements = new_data;
-  first_free = dest;
-  cap = elements + new_cap;
+
+  head = new_head;
+  rear = dest2constuct;
+  boundary = head + new_cap;
 
   return;
 }
 
 template <typename T>
 void Vec<T>::RangeInitialize(T const* first, T const* end) {
-  auto new_data{AllocNCopy(first, end)};
-  elements = new_data.first;
-  first_free = cap = new_data.second;
+  auto new_head{AllocNCopy(first, end)};
+  head = new_head.first;
+  rear = boundary = new_head.second;
   return;
 }
 
