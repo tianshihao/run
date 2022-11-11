30b377b71e1050f3149c1a90add806fe805282f7
Shihao Tian <shihao.tian@outlook.com>
2022-11-11 14:29:53 +0800

    feature(ex): producer and consumer

---
+++ ex/producer_consumer.cpp
@@ -0,0 +1,16 @@
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+
+#include "./factory_method.hpp"
+
+class Storage {};
+
+class Producer {
+ public:
+ private:
+};
+
+class Consumer {};
