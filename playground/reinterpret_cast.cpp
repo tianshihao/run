3334b9def1382450a95f7b7d742693963bcaa946
tianshihao <shihao.tian@outlook.com>
2023-07-16 21:06:09 +0800

    feature(playground): review some basic points

--- playground/reinterpret_cast.cpp
+++ playground/reinterpret_cast.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+namespace playground::reinterpret_cast_example {
 struct MyStruct {
   int x;
   int y;
@@ -7,7 +8,7 @@ struct MyStruct {
   bool b;
 };
 
-int main(int argc, char** argv) {
+void reinterpret_cast_example() {
   MyStruct s;
   s.x = 1;
   s.y = 2;
@@ -24,7 +25,11 @@ int main(int argc, char** argv) {
             << std::endl;
   p++;
 
-  // std::cout << *p << std::endl;
+  std::cout << *p << std::endl;
+  // The operation above will explain the data start from the address of p as
+  // integer, however, the data is not integer, so the output is not expected.
+  // This is the reason why we need reinterpret_cast.
+
   char* cp{reinterpret_cast<char*>(p)};
   std::cout << "charactor c: " << *cp << ", size of charactor: " << sizeof(*cp)
             << std::endl;
@@ -33,6 +38,12 @@ int main(int argc, char** argv) {
   bool* bp{reinterpret_cast<bool*>(cp)};
   std::cout << "bool b: " << *bp << ", size of bool: " << sizeof(*bp)
             << std::endl;
+}
+
+}  // namespace playground::reinterpret_cast_example
+
+int main(int argc, char** argv) {
+  playground::reinterpret_cast_example::reinterpret_cast_example();
 
   return 0;
 }
