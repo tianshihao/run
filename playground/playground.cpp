0dd9f1f291bddbe425cf3242c5d1b176aa2c0bec
tianshihao <shihao.tian@outlook.com>
2024-02-08 17:21:19 +0800

    feat(replica): MOS Ch2 process

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
-int main(int argc, char** argv) {
-  std::cout << "Hello, playground!" << std::endl;
+class A {
+ public:
+  A() = default;
+
+  void Print(int b) const {
+    std::cout << _a << '\n';
+    b = 2;
+    std::cout << b << '\n';
+  }
+
+ private:
+  int _a{0};
+};
+
+void Test() {
+  // [[maybe_unused]] A b;
+  [[maybe_unused]] A b;
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+  A a;
+  a.Print(1);
+
+  Test();
+
   return 0;
 }
